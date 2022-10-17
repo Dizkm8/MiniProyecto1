538b9feae1249da7760c2f6647f82cef8d086931
David Araya <david.araya@alumnos.ucn.cl>
2022-10-17 12:18:33 -0300

    feat: main updated to compile case three

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from CaseTwo import CaseTwo
+from CaseThree import CaseThree
 
 global data_frame
 
@@ -13,6 +14,13 @@ def solve_case_two():
     case_two.transform()
     case_two.load()
     print("Caso 2 realizado con éxito, información desplegada en Roberto.xlsx")
+
+
+def solve_case_three():
+    case_three = CaseThree(data_frame)
+    case_three.transform()
+    case_three.load()
+    print("Caso 3 realizado con éxito, información desplegada en Diana.xlsx")
 
 
 if __name__ == '__main__':
