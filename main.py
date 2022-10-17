f3ee2b7b664ce98af462a3cd8197a9f7731a44af
David Araya <david.araya@alumnos.ucn.cl>
2022-10-17 00:01:00 -0300

    refactor: dataframe updated to global declaration

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import pandas as pd
 from CaseTwo import CaseTwo
 
+global data_frame
+
 
 def extract():
     return pd.read_csv('resources/airbnb.csv', sep=',')
 
 
-if __name__ == '__main__':
-    case_two = CaseTwo(extract())
+def init_case_two():
+    case_two = CaseTwo(data_frame)
     case_two.transform()
+
+
+if __name__ == '__main__':
+    data_frame = extract()
+    init_case_two()
