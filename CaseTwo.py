e786d6cab9c9bb4b2931067992e952843953ce0a
David Araya <david.araya@alumnos.ucn.cl>
2022-10-17 00:34:59 -0300

    feat: Final update for case two

--- CaseTwo.py
+++ CaseTwo.py
@@ -1,6 +1,11 @@
 from typing import Final
 import pandas as pd
+import openpyxl
 
+
+# to_excel() function working https://datatofish.com/export-dataframe-to-excel/
+# the rest of methods: https://aprendeconalf.es/docencia/python/manual/pandas/#eliminar-columnas-de-un-dataframe
+# insert Columns function https://www.geeksforgeeks.org/python-pandas-dataframe-insert/
 
 class CaseTwo:
     def __init__(self, data_frame):
@@ -11,7 +16,11 @@ class CaseTwo:
     def transform(self):
         self.data_frame = self.data_frame[(self.data_frame['room_id'] == self.room_id_roberto) |
                                           (self.data_frame['room_id'] == self.room_id_clara)]
+        for i in ['host_id', 'room_type', 'neighborhood', 'accommodates', 'bedrooms', 'price']:
+            del self.data_frame[i]
+        self.data_frame.insert(0, 'Dueño', ['Clara', 'Roberto'], allow_duplicates=False)
+        self.data_frame = self.data_frame.rename(columns={'room_id': 'ID habitación', 'reviews': 'Cantidad reseñas',
+                                                          'overall_satisfaction': 'Puntuación (0 a 5)'})
 
     def load(self):
-
-        pass
+        self.data_frame.to_excel('Roberto.xlsx', sheet_name='Roberto', index=False, header=True)
--- main.py
+++ main.py
@@ -8,11 +8,13 @@ def extract():
     return pd.read_csv('resources/airbnb.csv', sep=',')
 
 
-def init_case_two():
+def solve_case_two():
     case_two = CaseTwo(data_frame)
     case_two.transform()
+    case_two.load()
+    print("Caso 2 realizado con éxito, información desplegada en Roberto.xlsx")
 
 
 if __name__ == '__main__':
     data_frame = extract()
-    init_case_two()
+    solve_case_two()
